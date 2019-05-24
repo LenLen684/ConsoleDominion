4f752daf1ab1554534c52759b0f188f4db27ea5a
Lene Lefiti <llefiti@student.neumont.edu>
2019-05-24 14:36:22 -0600

    Militia, Moat, Village
    Village is done, the rest are done until further notice.

--- ConsoleDominion/src/models/Militia.java
+++ ConsoleDominion/src/models/Militia.java
@@ -8,14 +8,13 @@ public class Militia extends Card {
 	public Militia() {
 		super("MILITIA", CardType.ACTION, SubType.ATTACK, 4);
 	}
-
+	//Done until I'm able to identify when the card is played and
+	//we have to go through all the other players
 	@Override
 	public void action(Player player) {
-		
-		
-		
+		player.setTreasure(player.getTreasure() + 2);
 	}
-	
+
 	@Override
 	public String toString() {
 		StringBuilder builder = new StringBuilder();
