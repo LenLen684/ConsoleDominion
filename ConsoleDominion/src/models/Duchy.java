c044a66c29e785643f0fb7cb905cb7f4a2957daa
randomizerkatt <ABroderick@student.neumont.edu>
2019-05-30 18:29:57 -0600

    VictoryCards toString
    All VictoryCards toStrings are created

--- ConsoleDominion/src/models/Duchy.java
+++ ConsoleDominion/src/models/Duchy.java
@@ -6,4 +6,25 @@ public class Duchy extends Victory {
 		super("DUCHY", null, 5, 3);
 	}
 
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("\r\n" + 
+				"  _____________________________\r\n" + 
+				" /            DUCHY  	       \\\r\n" + 
+				"|     	             	        |\r\n" + 
+				"|           3 Points	        |\r\n" + 
+				"|	  	      		|\r\n" + 
+				"|    				|\r\n" + 
+				"|-------------------------------|\r\n" + 
+				"|				|\r\n" + 
+				"|				|\r\n" + 
+				"|				|\r\n" + 
+				"|				|\r\n" + 
+				"|				|\r\n" + 
+				"|  cost 5     		Victory |\r\n" + 
+				" \\_____________________________/\r\n" + 
+				"");
+		return builder.toString();
+	}
 }
--- ConsoleDominion/src/models/Estate.java
+++ ConsoleDominion/src/models/Estate.java
@@ -6,4 +6,25 @@ public class Estate extends Victory {
 		super("ESTATE", null, 2, 1);
 	}
 
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("\r\n" + 
+				"  _____________________________\r\n" + 
+				" /            ESTATE  	       \\\r\n" + 
+				"|     	             	        |\r\n" + 
+				"|           1 Point	        |\r\n" + 
+				"|	  	      		|\r\n" + 
+				"|    				|\r\n" + 
+				"|-------------------------------|\r\n" + 
+				"|				|\r\n" + 
+				"|				|\r\n" + 
+				"|				|\r\n" + 
+				"|				|\r\n" + 
+				"|				|\r\n" + 
+				"|  cost 2     		Victory |\r\n" + 
+				" \\_____________________________/\r\n" + 
+				"");
+		return builder.toString();
+	}
 }
